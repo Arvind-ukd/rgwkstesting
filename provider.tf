64ac1eca5e241fe3c7dc01d8264e138a6edaef34
arvindng88 <arvindng88@gmail.com>
2023-01-05 15:06:31 +0530

    secret vault

--- provider.tf
+++ provider.tf
@@ -13,5 +13,5 @@ provider "azurerm" {
    subscription_id   = "05221eb3-62ea-4448-bd50-220757debb83"
   tenant_id         = "012c7f08-9121-41cf-a656-927814549a5c"
   client_id         = "4015cafc-80b1-4a82-9b7d-d56ec55098f4"
-  client_secret     = "b17498f2-de29-4908-a056-1238adaf4f6d"
+  client_secret     = "blO8Q~DWuccJNav9qUH8qbSqKxvECaGcRT9NGacM"
 }
